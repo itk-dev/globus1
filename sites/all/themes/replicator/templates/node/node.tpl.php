ee3af460ac108445999daa6909199afe876646b0
Brian Jensen <mail@magnify.dk>
2011-01-31 17:18:28 +0100

    Added Mail Chimp

--- sites/all/themes/replicator/templates/node/node.tpl.php
+++ sites/all/themes/replicator/templates/node/node.tpl.php
@@ -56,17 +56,8 @@
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
 <?php endif; ?>
 
-  <div class="meta">
-  <?php if ($submitted): ?>
-    <span class="submitted"><?php print $submitted ?></span>
-  <?php endif; ?>
-
-  <?php if ($terms): ?>
-    <div class="terms terms-inline"><?php print $terms ?></div>
-  <?php endif;?>
-  </div>
-
   <div class="content">
+    <h1 class="page-title"><?php print $title ?></h1>
     <?php print $content ?>
   </div>
 
